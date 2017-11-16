8b223c9fd047702c0b278cd10e24f162fb69ee76
mar <thayshi@yandex.ru>
2017-11-16 13:33:34 +0300

    переписала на одну переменную

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-let menu = document.querySelector('.main__menu').children;
+let menu = document.querySelector('.main__menu');
 
-for (let i = 0; i < menu.length; i++) {
-    console.log(this);
-    menu[i].addEventListener('click', function(e) {
-        for (let prop of menu) {
-            if (prop != menu[i]) { prop.classList.remove('active'); }
+for (let i = 0; i < menu.children.length; i++) {
+    menu.children[i].addEventListener('click', function(e) {
+        for (let prop of menu.children) {
+            if (prop != menu.children[i]) { prop.classList.remove('active'); }
         }
         (this.classList.contains('active')) ? this.classList.remove('active'): this.classList.add('active');
     });
 }
 
 document.querySelector('.toggle').addEventListener('click', function() {
-    let mainMenu = document.querySelector('.main__menu');
-    mainMenu.style.display = (mainMenu.style.display == 'block') ? 'none' : 'block';
+    menu.style.display = (menu.style.display == 'block') ? 'none' : 'block';
 });
